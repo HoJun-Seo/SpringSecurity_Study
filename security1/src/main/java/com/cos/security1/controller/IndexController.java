0703d76880cde58e481f7f8c7165d574b183648e
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-09-24 16:55:37 +0900

    Spring security

--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -12,5 +13,41 @@ public class IndexController {
         // View 파일을 찾는 기본적인 경로는 src/main/resources/ 로 잡혀있다.
         // 뷰 리졸버 경로는 templates(prefix), .mustache(suffix) 로 잡혀있다.
         return "index";
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    @ResponseBody
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    @ResponseBody
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    @ResponseBody
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    @ResponseBody
+    public String joinProc() {
+        return "회원가입 완료됨";
     }
 }
