44baee913c7ea946205af6d7cabb1194bcc77528
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-11-10 19:31:42 +0900

    Spring Security

--- jwt/src/main/java/com/cos/jwt/config/filter/JwtAuthenticationFilter.java
+++ jwt/src/main/java/com/cos/jwt/config/filter/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.cos.jwt.config.filter;
 
 import java.io.IOException;
+import java.util.Date;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -8,6 +9,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.cos.jwt.config.auth.PrincipalDetails;
 import com.cos.jwt.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -66,6 +69,22 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
             Authentication authResult) throws IOException, ServletException {
         System.out.println("successfulAuthentication 실행됨 : 인증 완료");
-        super.successfulAuthentication(request, response, chain, authResult);
+
+        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+
+        // RSA 방식은 아니고 Hash 암호방식
+        String jwtToken = JWT.create()
+                .withSubject("cos 토큰")
+                .withExpiresAt(new Date(System.currentTimeMillis() + (60000 * 10))) // 토큰 만료시간, 시간을 짧게 해줘야 탈취가 되도
+                                                                                    // 공격허용
+                                                                                    // 시간을 최소화 시킬 수 있다.
+                                                                                    // (60,000 * 10 : 10분) -> 이 시간이 지나면
+                                                                                    // 토큰을 새로 만들어주어야 한다.
+                .withClaim("id", principalDetails.getUser().getId())
+                .withClaim("username", principalDetails.getUser().getUsername())
+                .sign(Algorithm.HMAC512("cos")); // sign 은 현재 서버만 알고 있는 고유한 값이어야함
+        // withClaim 은 비공개 클레임인데 여기는 넣고싶은 key, value 값을 아무렇게나 넣어도 된다.
+
+        response.addHeader("Authorization", "Bearer " + jwtToken); // 응답 헤더에 토큰 담아주기
     }
 }
